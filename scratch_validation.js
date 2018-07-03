ede9fc40f804f1db42ebfae86ba4e47b26023a2b
MaeganW <maeganwomble@gmail.com>
2018-07-03 13:13:57 -0500

    validation function working

---
+++ scratch_validation.js
@@ -0,0 +1,25 @@
+// extracting the required fields data
+(function () {
+  var requiredFields = [];
+  data.forEach(function (schema) {
+    if (schema.required) {
+      requiredFields.push(schema.schemaName)
+    }
+    return;
+  })
+  return requiredFields
+}())
+
+
+
+  // validation - returns true or false
+  (function () {
+    var getValidationStatus = function () {
+      return requiredFields.every(function (requiredField) {
+        return mappedHeadings.mappedHeadings.find(function (heading) {
+          return heading.group === requiredField
+        })
+      })
+    }
+    return getValidationStatus();
+  }())
