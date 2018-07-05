e6a3ea390442b44b5e034937b5b32cdbd728a946
MaeganW <maeganwomble@gmail.com>
2018-07-05 15:27:22 -0500

    wip

--- BooleanToggledPopup/BooleanToggledPopup.js
+++ BooleanToggledPopup/BooleanToggledPopup.js
@@ -1,6 +1,73 @@
 export default {
   _init() {
 
+    this._pop_up();
+
+  },
+
+  target() {
+
+    this._pop_up();
+
+  },
+
+  options() {
+
+    this._pop_up();
+
+  },
+
+  _pop_up() {
+
+    let state = this.api.inputState.export(),
+      layout = this.api.layoutElement,
+      target = state.target,
+      options = state.options,
+      $ = this.api.imports.$;
+
+    $(target)
+      .popup({
+        inline: options.inline,
+        hoverable: options.hoverable,
+        popup: $(layout),
+        position: options.position,
+        on: false
+      });
+
+  },
+
+  toggle() {
+
+    let state = this.api.inputState.export(),
+      layout = this.api.layoutElement,
+      target = state.target,
+      isHidden = state.toggle,
+      $ = this.api.imports.$;
+
+    if (!isHidden) {
+      $(target)
+        .popup('show');
+      this.api.output("opened", true);
+    } else {
+      $(target)
+        .popup('hide');
+      this.api.output("closed", true);
+    }
+
+  }
+
+};
+
+
+
+
+
+
+
+// Original Component Code
+export default {
+  _init() {
+
     this.isOpen = false;
     this._pop_up();
 
@@ -42,10 +109,9 @@ export default {
     let state = this.api.inputState.export(),
       layout = this.api.layoutElement,
       target = state.target,
-      isHidden = state.toggle;
-    $ = this.api.imports.$;
+      $ = this.api.imports.$;
 
-    if (!this.isHidden) {
+    if (!this.isOpen) {
       $(target)
         .popup('show');
       this.isOpen = true;
