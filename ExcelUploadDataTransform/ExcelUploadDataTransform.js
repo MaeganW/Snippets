6ba122a94d045b73887ffda41de6d6c1ee940952
MaeganW <maeganwomble@gmail.com>
2018-07-12 17:43:17 -0500

    wip

--- ExcelUploadDataTransform/ExcelUploadDataTransform.js
+++ ExcelUploadDataTransform/ExcelUploadDataTransform.js
@@ -94,27 +94,96 @@ export default {
 
     this.transformData = function () {
       var eventData = _this.api.inputState.export().data;
+      var axios = _this.api.imports.axios;
       var hasError = false;
       console.log('the event data', eventData);
 
       ///////////////////////////////////////////////////////////////
 
       function getTransformedData() {
-        return eventData.map(function (event) {
+        var goodData = [];
+        var badData = [];
+
+        eventData.forEach(function (event) {
           var encodedAddress = encodeURI(event.venue_address);
-          console.log('venue address', encodedAddress)
           var transformedEventData = event;
+
           getGeocodedAddress(encodedAddress).then(function (geo) {
             console.log('geo', geo);
             transformedEventData.venue_lat = geo.results[0].geometry.location.lat;
             transformedEventData.venue_lon = geo.results[0].geometry.location.lng;
-            return transformedEventData;
+            goodData.push(transformedEventData);
           }).catch(function (err) {
             console.log('error ', err);
             hasError = true;
+            badData.push(event);
             _this.api.output("error", err.toString());
-            return event;
           });
+
+        });
+      }
+
+      function getGeocodedAddress(address) {
+        var url = 'https://maps.googleapis.com/maps/api/geocode/json?address=';
+        var apiKey = '';
+        return axios.get(`${url}${address}&key=${apiKey}`);
+      }
+
+      _this.api.output("transformedData", getTransformedData());
+      _this.api.output("success", !hasError);
+    };
+  },
+  _close() {
+  },
+  data() {
+    this.transformData();
+  }
+};
+
+
+
+
+
+
+
+export default {
+  _init() {
+    var _this = this;
+
+    this.transformData = function () {
+      var eventData = _this.api.inputState.export().data;
+      var hasError = false;
+      console.log('the event data', eventData);
+
+      ///////////////////////////////////////////////////////////////
+
+      function getTransformedData() {
+        var goodData = [];
+        var badData = [];
+        var length = eventData.length;
+        var tracker = 0;
+
+        eventData.forEach(function (event) {
+          var encodedAddress = encodeURI(event.venue_address);
+          var transformedEventData = event;
+
+          getGeocodedAddress(encodedAddress).then(function (geo) {
+            console.log('geo', geo);
+            transformedEventData.venue_lat = geo.results[0].geometry.location.lat;
+            transformedEventData.venue_lon = geo.results[0].geometry.location.lng;
+            goodData.push(transformedEventData);
+            tracker++;
+          }).catch(function (err) {
+            console.log('error ', err);
+            hasError = true;
+            badData.push(event);
+            tracker++;
+            _this.api.output("error", err.toString());
+          });
+
+          setInterval(function check() {
+
+          })
         });
       }
 
@@ -159,3 +228,91 @@ export default {
 //     return event;
 //   });
 // }())
+
+
+
+
+
+
+
+
+
+// eventData.forEach(function (event) {
+//   var encodedAddress = encodeURI(event.venue_address);
+//   var transformedEventData = event;
+
+//   getGeocodedAddress(encodedAddress).then(function (geo) {
+//     console.log('geo', geo);
+//     transformedEventData.venue_lat = geo.results[0].geometry.location.lat;
+//     transformedEventData.venue_lon = geo.results[0].geometry.location.lng;
+//     goodData.push(transformedEventData);
+//   }).catch(function (err) {
+//     console.log('error ', err);
+//     hasError = true;
+//     badData.push(event);
+//     _this.api.output("error", err.toString());
+//   });
+
+// });
+
+
+
+
+
+
+
+
+
+export default {
+  _init() {
+    var _this = this;
+
+    this.transformData = function () {
+      var eventData = _this.api.inputState.export().data;
+      var axios = _this.api.imports.axios;
+      var hasError = false;
+      console.log('the event data', eventData);
+
+      ///////////////////////////////////////////////////////////////
+
+      function getTransformedData() {
+        var goodData = [];
+        var badData = [];
+        var eventsArrayWithPromises = eventData.map(function (event) {
+          event.promise = buildGeocodeGetRequest(event.venue_address)
+          return event;
+        })
+        var chunkedPromises = chunkArray(eventsArrayWithPromises, 10);
+        var fullDataCount = eventData.length;  // events count
+        var fullChunkCount = chunkedPromises.length;  // event clusters count
+
+        initiateAllPromises(chunkedPromises);
+
+
+      }
+
+      function chunkArray(myArray, chunk_size) {
+        var results = [];
+        while (myArray.length) {
+          results.push(myArray.splice(0, chunk_size));
+        }
+        return results;
+      }
+
+      function buildGeocodeGetRequest(address) {
+        var encodedAddress = encodeURI(address);
+        var url = 'https://maps.googleapis.com/maps/api/geocode/json?address=';
+        var apiKey = '';
+        return axios.get(`${url}${encodedAddress}&key=${apiKey}`);
+      }
+
+      _this.api.output("transformedData", getTransformedData());
+      _this.api.output("success", !hasError);
+    };
+  },
+  _close() {
+  },
+  data() {
+    this.transformData();
+  }
+};
