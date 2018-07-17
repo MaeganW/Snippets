9d2ec08c65c7bd3690b5b1c81fb51c14f4d13086
MaeganW <maeganwomble@gmail.com>
2018-07-17 17:22:06 -0500

    date parser revamped - matches date formats in db - new functions for validating

--- DateParser/dateParser.js
+++ DateParser/dateParser.js
@@ -14,7 +14,7 @@ export default {
         _this.api.output("loading", true);
         let parsedData = null;
         if (eventData) {
-          parsedData = sortData(parseDates(eventData));
+          parsedData = parseDates(eventData);
         }
 
         _this.api.output("hasError", hasError);
@@ -26,47 +26,6 @@ export default {
 
       // format dates on the events
       function parseDates(events) {
-        let badData = [];
-        let goodData = [];
-        let badDataUnedited = [];
-
-        events.forEach(event => {
-          if (!checkValidDates(event)) {
-            return badData.push(event);
-          }
-
-          event.time_start = Date.parse(event.event_date); //relies on event date being required
-          event.time_end = Date.parse(event.event_date); //relies on event date being required
-          event.event_date = Date.parse(event.event_date); //relies on event date being required
-
-          if (event.venue_start_time && validateTime(event.venue_start_time)) {
-            event.venue_start_time = Date.parse(event.venue_start_time);
-          } else {
-            event.venue_start_time = Date.parse(event.event_date);
-          }
-
-          if (event.venue_end_time && validateTime(event.venue_end_time)) {
-            event.venue_end_time = Date.parse(event.venue_end_time);
-          } else {
-            event.venue_end_time = Date.parse(event.event_date);
-          }
-
-          return goodData.push(event);
-        });
-      }
-
-      // returns a boolean - checks if date parsing returns valid data
-      function validateTime(prop) {
-        const parsedDate = Date.parse(prop);
-        if (typeof parsedDate !== 'number' || Object.is(parsedDate, NaN)) {
-          hasError = true;
-          return false;
-        }
-        return true;
-      }
-
-      // check valid dates
-      function checkValidDates(event) {
         const timeProps = [
           'venue_end_time',
           'venue_start_time',
@@ -74,134 +33,46 @@ export default {
           'time_start',
           'event_date'
         ];
-
-        if (timeProps.some(prop => Object.is(event[prop], NaN) || typeof event[prop] !== "number")) {
-          hasError = true;
-          return false;
-        } else {
-          return true;
-        };
-      }
-
-      // sort out the good data from the bad
-      function sortData(events) {
         let badData = [];
         let goodData = [];
 
         events.forEach(event => {
-          const timeProps = [
-            'venue_end_time',
-            'venue_start_time',
-            'time_end',
-            'time_start',
-            'event_date'
-          ];
-
           if (timeProps.some(prop => Object.is(event[prop], NaN) || typeof event[prop] !== "number")) {
             hasError = true;
             badData.push(event);
           } else {
-            goodData.push(event);
+
+            // this object will only contain times
+            let timeObj = {};
+
+            // ex. "7/4/2018"
+            timeObj.time_start = Date.parse(event.time_start);
+            timeObj.time_end = Date.parse(event.time_end);
+            // returns 1530680400000
+
+            // ex. "7/4/2018 6:00 PM"
+            timeObj.venue_start_time = moment(event.venue_start_time).format();
+            timeObj.venue_end_time = moment(event.venue_end_time).format();
+            // returns "2018-07-04T18:00:00-05:00"
+
+            // ex. 7/4/2018
+            timeObj.event_date = moment(event.event_date).format('YYYY-MM-DD');
+            // returns "2018-07-04"
+
+            if (timeProps.some(prop => Object.is(timeObj[prop], NaN) || typeof timeObj[prop] !== "number")) {
+              hasError = true;
+              badData.push(event);
+            } else {
+              timeProps.forEach(prop => event[prop] = timeObj[prop])
+              goodData.push(event);
+            }
           }
         });
 
         return {
-          badData,
-          goodData
-        };
-      }
-    };
-  },
-  _close() {
-  },
-  data() {
-    this.transformData();
-  }
-};
-
-
-
-
-
-
-
-
-// For the date parsing component
-
-export default {
-  _init() {
-    const _this = this;
-
-    this.transformData = function () {
-      const eventData = _this.api.inputState.export().data;
-      let hasError = false;
-      getTransformedData();
-
-      ///////////////////////////////////////////////////////////////
-
-      function getTransformedData() {
-        _this.api.output("loading", true);
-        let parsedData = null;
-        if (eventData) {
-          parsedData = sortData(parseDates(eventData));
+          goodData,
+          badData
         }
-
-        _this.api.output("hasError", hasError);
-        _this.api.output("transformedData", (!hasError && parsedData) ? parsedData.goodData : null);
-        _this.api.output("loading", false);
-        _this.api.output("goodData", parsedData ? parsedData.goodData : null);
-        _this.api.output("badData", parsedData ? parsedData.badData : null);
-      }
-
-      // format dates on the events
-      function parseDates(events) {
-        return events.map(event => {
-          event.time_start = Date.parse(event.event_date); //relies on event date being required
-          event.time_end = Date.parse(event.event_date); //relies on event date being required
-          event.event_date = Date.parse(event.event_date); //relies on event date being required
-
-          if (event.venue_start_time) {
-            event.venue_start_time = Date.parse(event.venue_start_time);
-          } else {
-            event.venue_start_time = Date.parse(event.event_date);
-          }
-
-          if (event.venue_end_time) {
-            event.venue_end_time = Date.parse(event.venue_end_time);
-          } else {
-            event.venue_end_time = Date.parse(event.event_date);
-          }
-
-          return event;
-        });
-      }
-
-      // sort out the good data from the bad
-      function sortData(events) {
-        let badData = [];
-        let goodData = [];
-
-        events.forEach(event => {
-          const timeProps = [
-            'venue_end_time',
-            'venue_start_time',
-            'time_end',
-            'time_start',
-            'event_date'
-          ];
-
-          if (timeProps.some(prop => Object.is(event[prop], NaN) || typeof event[prop] !== "number")) {
-            hasError = true;
-            badData.push(event);
-          } else {
-            goodData.push(event);
-          }
-        });
-
-        return {
-          badData,
-          goodData
-        };
       }
     };
   },
@@ -211,3 +82,248 @@ export default {
     this.transformData();
   }
 };
+
+
+
+
+
+
+
+
+
+// // Before cleanup
+
+// export default {
+//   _init() {
+//     const _this = this;
+
+//     this.transformData = function () {
+//       const eventData = _this.api.inputState.export().data;
+//       let hasError = false;
+//       getTransformedData();
+
+//       ///////////////////////////////////////////////////////////////
+
+//       function getTransformedData() {
+//         _this.api.output("loading", true);
+//         let parsedData = null;
+//         if (eventData) {
+//           parsedData = sortData(parseDates(eventData));
+//         }
+
+//         _this.api.output("hasError", hasError);
+//         _this.api.output("transformedData", (!hasError && parsedData) ? parsedData.goodData : null);
+//         _this.api.output("loading", false);
+//         _this.api.output("goodData", parsedData ? parsedData.goodData : null);
+//         _this.api.output("badData", parsedData ? parsedData.badData : null);
+//       }
+
+//       // format dates on the events
+//       function parseDates(events) {
+//         let badData = [];
+//         let goodData = [];
+//         const timeProps = [
+//           'venue_end_time',
+//           'venue_start_time',
+//           'time_end',
+//           'time_start',
+//           'event_date'
+//         ];
+
+//         events.forEach(event => {
+//           if (timeProps.some(prop => Object.is(event[prop], NaN) || typeof event[prop] !== "number")) {
+//             badData.push(event);
+//           } else {
+//             // this object will only contain times
+//             let timeObj = {};
+
+//             // ex. "7/4/2018"
+//             timeObj.time_start = Date.parse(event.time_start);
+//             timeObj.time_end = Date.parse(event.time_end);
+//             // returns 1530680400000
+
+//             // ex. "7/4/2018 6:00 PM"
+//             timeObj.venue_start_time = moment(event.venue_start_time).format()
+//             timeObj.venue_end_time = moment(event.venue_end_time).format()
+//             // returns "2018-07-04T18:00:00-05:00"
+
+//             // ex. 7/4/2018
+//             timeObj.event_date = moment(event.event_date).format('YYYY-MM-DD');
+//             // returns "2018-07-04"
+
+//             if (timeProps.some(prop => Object.is(timeObj[prop], NaN) || typeof timeObj[prop] !== "number")) {
+//               badData.push(event);
+//             } else {
+//               timeProps.forEach(prop => event[prop] = timeObj[prop])
+//               goodData.push(event);
+//             }
+//           }
+//         });
+//       }
+
+//       // returns a boolean - checks if date parsing returns valid data
+//       function validateTime(prop) {
+//         const parsedDate = Date.parse(prop);
+//         if (typeof parsedDate !== 'number' || Object.is(parsedDate, NaN)) {
+//           hasError = true;
+//           return false;
+//         }
+//         return true;
+//       }
+
+//       // check valid dates
+//       function checkValidDates(event) {
+//         const timeProps = [
+//           'venue_end_time',
+//           'venue_start_time',
+//           'time_end',
+//           'time_start',
+//           'event_date'
+//         ];
+
+//         if (timeProps.some(prop => Object.is(event[prop], NaN) || typeof event[prop] !== "number")) {
+//           hasError = true;
+//           return false;
+//         } else {
+//           return true;
+//         };
+//       }
+
+//       // sort out the good data from the bad
+//       function sortData(events) {
+//         let badData = [];
+//         let goodData = [];
+
+//         events.forEach(event => {
+//           const timeProps = [
+//             'venue_end_time',
+//             'venue_start_time',
+//             'time_end',
+//             'time_start',
+//             'event_date'
+//           ];
+
+//           if (timeProps.some(prop => Object.is(event[prop], NaN) || typeof event[prop] !== "number")) {
+//             hasError = true;
+//             badData.push(event);
+//           } else {
+//             goodData.push(event);
+//           }
+//         });
+
+//         return {
+//           badData,
+//           goodData
+//         };
+//       }
+//     };
+//   },
+//   _close() {
+//   },
+//   data() {
+//     this.transformData();
+//   }
+// };
+
+
+
+
+
+// // {
+// //   time_end: 1531883100000,
+// //   time_start: 1531872300000,
+// //   event_date: "2018-07-17",
+// //   venue_end_time: "2018-07-18T03:05:00+00:00",
+// //   venue_start_time: "2018-07-18T00:05:00+00:00",
+// // }
+
+
+
+
+
+
+// // For the date parsing component
+
+// export default {
+//   _init() {
+//     const _this = this;
+
+//     this.transformData = function () {
+//       const eventData = _this.api.inputState.export().data;
+//       let hasError = false;
+//       getTransformedData();
+
+//       ///////////////////////////////////////////////////////////////
+
+//       function getTransformedData() {
+//         _this.api.output("loading", true);
+//         let parsedData = null;
+//         if (eventData) {
+//           parsedData = sortData(parseDates(eventData));
+//         }
+
+//         _this.api.output("hasError", hasError);
+//         _this.api.output("transformedData", (!hasError && parsedData) ? parsedData.goodData : null);
+//         _this.api.output("loading", false);
+//         _this.api.output("goodData", parsedData ? parsedData.goodData : null);
+//         _this.api.output("badData", parsedData ? parsedData.badData : null);
+//       }
+
+//       // format dates on the events
+//       function parseDates(events) {
+//         return events.map(event => {
+//           event.time_start = Date.parse(event.event_date); //relies on event date being required
+//           event.time_end = Date.parse(event.event_date); //relies on event date being required
+//           event.event_date = Date.parse(event.event_date); //relies on event date being required
+
+//           if (event.venue_start_time) {
+//             event.venue_start_time = Date.parse(event.venue_start_time);
+//           } else {
+//             event.venue_start_time = Date.parse(event.event_date);
+//           }
+
+//           if (event.venue_end_time) {
+//             event.venue_end_time = Date.parse(event.venue_end_time);
+//           } else {
+//             event.venue_end_time = Date.parse(event.event_date);
+//           }
+
+//           return event;
+//         });
+//       }
+
+//       // sort out the good data from the bad
+//       function sortData(events) {
+//         let badData = [];
+//         let goodData = [];
+
+//         events.forEach(event => {
+//           const timeProps = [
+//             'venue_end_time',
+//             'venue_start_time',
+//             'time_end',
+//             'time_start',
+//             'event_date'
+//           ];
+
+//           if (timeProps.some(prop => Object.is(event[prop], NaN) || typeof event[prop] !== "number")) {
+//             hasError = true;
+//             badData.push(event);
+//           } else {
+//             goodData.push(event);
+//           }
+//         });
+
+//         return {
+//           badData,
+//           goodData
+//         };
+//       }
+//     };
+//   },
+//   _close() {
+//   },
+//   data() {
+//     this.transformData();
+//   }
+// };
---
+++ DateParser/exampleData.js
@@ -0,0 +1,38 @@
+{
+  venue_state: "OK",
+    _score: null,
+      time_end: 1531883100000,
+        venue_city: "Oklahoma City",
+          id: 27266117,
+            venue_lat: 35.465,
+              venue_start_time: "2018-07-18T00:05:00+00:00",
+                title: "Oklahoma City Dodgers vs. New Orleans Baby Cakes",
+                  event_id: 27266117,
+                    venue_address: "303 E Sheridan Ave",
+                      source: "Facebook",
+                        venue_zip: "73104",
+                          event_date: "2018-07-17",
+                            sort: [
+                              1531785600000
+                            ],
+                              _type: "okccvb",
+                                description: "Sale Dates and Times: Public Onsale : Wed, 21 Mar 2018 at 10:00 AM",
+                                  tags: [
+                                    "Competition/Race",
+                                    "Mens",
+                                    "Sports & Fitness"
+                                  ],
+                                    venue_end_time: "2018-07-18T03:05:00+00:00",
+                                      time_start: 1531872300000,
+                                        url_logo: "http://res.cloudinary.com/chivvy/image/upload/v1521539777/vaiuxwd1g6j37a3nzez1.jpg",
+                                          url: "https://www.facebook.com/events/844003982449686",
+                                            venue_lon: -97.508,
+                                              venue_name: "Chickasaw Bricktown Ballpark",
+                                                districts: [
+                                                  "Bricktown"
+                                                ],
+                                                  _index: "chivvy",
+                                                    status: "not reviewed",
+                                                      date_created: "2018-03-20 05:34:58",
+                                                        _id: "27266117"
+}
