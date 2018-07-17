e325c34eca385973341b19601a701e57d9059070
MaeganW <maeganwomble@gmail.com>
2018-07-17 17:27:31 -0500

    schema

---
+++ MergeGates/schema.js
@@ -0,0 +1,66 @@
+[
+  {
+    "schemaName": "venue_state",
+    "required": false
+  },
+  {
+    "schemaName": "venue_city",
+    "required": false
+  },
+  {
+    "schemaName": "event_id",
+    "required": true
+  },
+  {
+    "schemaName": "venue_start_time",
+    "required": true
+  },
+  {
+    "schemaName": "venue_end_time",
+    "required": false
+  },
+  {
+    "schemaName": "event_date",
+    "required": true
+  },
+  {
+    "schemaName": "time_start",
+    "required": false
+  },
+  {
+    "schemaName": "time_end",
+    "required": false
+  },
+  {
+    "schemaName": "title",
+    "required": true
+  },
+  {
+    "schemaName": "description",
+    "required": false
+  },
+  {
+    "schemaName": "venue_name",
+    "required": false
+  },
+  {
+    "schemaName": "venue_address",
+    "required": true
+  },
+  {
+    "schemaName": "venue_zip",
+    "required": false
+  },
+  {
+    "schemaName": "url_logo",
+    "required": false
+  },
+  {
+    "schemaName": "url",
+    "required": false
+  },
+  {
+    "schemaName": "tags",
+    "required": true
+  }
+]
